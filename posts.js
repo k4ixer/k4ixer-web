3b9895231c932cb912f5ab894263ddfbf37f1434
Alejandro Herrero <146971605+k4ixer@users.noreply.github.com>
2025-02-01 13:12:50 +0100

    Nuevo post

--- posts.js
+++ posts.js
@@ -1,9 +1,16 @@
 const posts = [ //Aplicar en orden correcto, poner los nuevos post al principio del todo
     {
+        titulo: "CyberHeroes Writeup",
+        fecha: "1 de febrero de 2025",
+        url: "/posts/CyberHeroes.html",
+        contenido: "<p>Writeup de la maquina CyberHeroes (TryHackMe)</p>",
+        tipo: "writeup" //writeup, video, auncio, post
+    },
+    {
         titulo: "Presentación de mi pagina web",
         fecha: "31 de enero de 2025",
         url: "/posts/bienvenido.html",
         contenido: "<p>Bienvenidos a mi pagina web sobre ciberseguridad</p>",
         tipo: "anuncio" //writeup, video, auncio, post
-    }   
+    }  
 ];
