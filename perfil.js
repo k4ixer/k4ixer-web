c820e99dbad41c1224f5e41c28cc11587e276d7a
Alejandro Herrero <146971605+k4ixer@users.noreply.github.com>
2025-02-01 11:42:30 +0100

    Resubir la web entera

---
+++ perfil.js
@@ -0,0 +1,8 @@
+const perfil = {
+    nombre: "Alejandro Herrero",
+    sobre_mi: "Soy Alejandro Herrero, un joven de 16 años de España, apasionado por la ciberseguridad. Me gusta crear herramientas en Bash para contribuir a la comunidad y compartir mis conocimientos.",
+    certificaciones: ["Preparandome eJPT"],
+    redes: {
+        GitHub: "https://github.com/k4ixer"
+    }
+};
---
+++ posts.js
@@ -0,0 +1,9 @@
+const posts = [ //Aplicar en orden correcto, poner los nuevos post al principio del todo
+    {
+        titulo: "Presentación de mi pagina web",
+        fecha: "31 de enero de 2025",
+        url: "/posts/bienvenido.html",
+        contenido: "<p>Bienvenidos a mi pagina web sobre ciberseguridad</p>",
+        tipo: "anuncio" //writeup, video, auncio, post
+    }   
+];
